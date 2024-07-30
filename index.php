7368e91ed8f9dc16a4c14db839f981f01064ef08
caleadar <63838224+1426239465@users.noreply.github.com>
2024-07-30 17:30:47 +0800

    Update index.php
    远程调用本地化不走cdn.jsdelivr.net，去除购买弹窗

--- index.php
+++ index.php
@@ -607,7 +607,7 @@ class U {
   private static $assetspath;
   public static function assetspath(){
     if(self::$assetspath) return self::$assetspath;
-    return self::$assetspath = Config::get('assets') ? rtrim(Config::get('assets'), '/') . '/' : '_files/assets/';
+    return self::$assetspath = Config::get('assets') ? rtrim(Config::get('assets'), '/') . '/' : '_files/assets/';//修改本地化位置1
   }
 
   // response headers
@@ -2562,7 +2562,7 @@ if(U::get('action')){
     // various requirements, which would normally be satisfied if accessed from the interface
     $version = $request->param('version');
     if(!$version || !Config::get('allow_check_updates') || version_compare($version, Config::$version) <= 0 || !is_writable(__FILE__)) $request->error('Error');
-    $get = @file_get_contents('_files/assets/files.photo.gallery@' . $version . '/index.php');
+    $get = @file_get_contents('_files/assets/files.photo.gallery@' . $version . '/index.php');//修改本地化位置2
     if(empty($get) || strpos($get, '<?php') !== 0 || !@file_put_contents(__FILE__, $get)) Json::error('failed to update');
     Json::jexit(['success' => true]);
 
